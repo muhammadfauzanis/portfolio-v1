034396a43005626e7c4141681ac8dda974db1363
mfauzann982 <mfauzann982@gmail.com>
2024-11-29 08:55:49 +0700

    update a few things

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 export default function Footer() {
   return (
     <footer className="mb-5 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2024 Frontend Advance . All rights reserved.
+        &copy; 2024 Muhammad Fauzan . All rights reserved.
       </small>
     </footer>
   );
